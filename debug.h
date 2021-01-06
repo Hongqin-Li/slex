fe522087a05a1ce7fe783277ba4fa9e167858566
Hongqin-Li <ihongqinli@gmail.com>
2021-01-06 12:22:49 +0800

    chore: fix debug header

--- debug.h
+++ debug.h
@@ -4,33 +4,88 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template<typename T1, typename T2>
-string to_string(const pair<T1, T2>& p) {
-  return '(' + to_string(p.first) + ", " + to_string(p.second) + ')';
-}
+template <typename A, typename B>
+string to_string(pair<A, B> p);
+
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p);
+
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p);
+
 string to_string(const string& s) {
-  return '\"' + s + '\"';
+  return '"' + s + '"';
 }
 
-template<typename T>
-string to_string(T a) {
-  string res = "[";
-  int i = 0;
-  for (auto x: a) {
-    if (i != 0) res += ' ';
-    res += to_string(x);
-    i ++;
+string to_string(const char* s) {
+  return to_string((string) s);
+}
+
+string to_string(bool b) {
+  return (b ? "true" : "false");
+}
+
+string to_string(vector<bool> v) {
+  bool first = true;
+  string res = "{";
+  for (int i = 0; i < static_cast<int>(v.size()); i++) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string(v[i]);
   }
-  return res + "]";
+  res += "}";
+  return res;
 }
 
-void debug_out() { cerr << '\n'; }
-template<typename H, typename... T>
-void debug_out(H h, T... t) {
-  cerr << to_string(h) << ' ';
-  debug_out(t...);
+template <size_t N>
+string to_string(bitset<N> v) {
+  string res = "";
+  for (size_t i = 0; i < N; i++) {
+    res += static_cast<char>('0' + v[i]);
+  }
+  return res;
 }
 
-#define debug(...) cerr << "[" << #__VA_ARGS__ << "]: ", debug_out(__VA_ARGS__)
+template <typename A>
+string to_string(A v) {
+  bool first = true;
+  string res = "{";
+  for (const auto &x : v) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string(x);
+  }
+  res += "}";
+  return res;
+}
+
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+  return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p) {
+  return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")";
+}
+
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p) {
+  return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";
+}
+
+void debug_out() { cerr << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+  cerr << " " << to_string(H);
+  debug_out(T...);
+}
+
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
 
 #endif
