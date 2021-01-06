1dfc928617fefd5052e41d85fc001e4d348c3898
Hongqin-Li <ihongqinli@gmail.com>
2021-01-06 22:19:52 +0800

    chore: faster debug by const ref

--- debug.h
+++ debug.h
@@ -5,13 +5,13 @@
 using namespace std;
 
 template <typename A, typename B>
-string to_string(pair<A, B> p);
+string to_string(const pair<A, B>& p);
 
 template <typename A, typename B, typename C>
-string to_string(tuple<A, B, C> p);
+string to_string(const tuple<A, B, C>& p);
 
 template <typename A, typename B, typename C, typename D>
-string to_string(tuple<A, B, C, D> p);
+string to_string(const tuple<A, B, C, D>& p);
 
 string to_string(const string& s) {
   return '"' + s + '"';
@@ -25,7 +25,7 @@ string to_string(bool b) {
   return (b ? "true" : "false");
 }
 
-string to_string(vector<bool> v) {
+string to_string(const vector<bool>& v) {
   bool first = true;
   string res = "{";
   for (int i = 0; i < static_cast<int>(v.size()); i++) {
@@ -40,7 +40,7 @@ string to_string(vector<bool> v) {
 }
 
 template <size_t N>
-string to_string(bitset<N> v) {
+string to_string(const bitset<N>& v) {
   string res = "";
   for (size_t i = 0; i < N; i++) {
     res += static_cast<char>('0' + v[i]);
@@ -49,7 +49,7 @@ string to_string(bitset<N> v) {
 }
 
 template <typename A>
-string to_string(A v) {
+string to_string(const A& v) {
   bool first = true;
   string res = "{";
   for (const auto &x : v) {
@@ -64,24 +64,24 @@ string to_string(A v) {
 }
 
 template <typename A, typename B>
-string to_string(pair<A, B> p) {
+string to_string(const pair<A, B>& p) {
   return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
 }
 
 template <typename A, typename B, typename C>
-string to_string(tuple<A, B, C> p) {
+string to_string(const tuple<A, B, C>& p) {
   return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")";
 }
 
 template <typename A, typename B, typename C, typename D>
-string to_string(tuple<A, B, C, D> p) {
+string to_string(const tuple<A, B, C, D>& p) {
   return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";
 }
 
 void debug_out() { cerr << endl; }
 
 template <typename Head, typename... Tail>
-void debug_out(Head H, Tail... T) {
+void debug_out(const Head& H, Tail... T) {
   cerr << " " << to_string(H);
   debug_out(T...);
 }
